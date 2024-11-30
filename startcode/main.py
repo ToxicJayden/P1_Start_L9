0e72a3361c428e07c33aefe546cfb00fb6f84048
ToxicJayden <jayden.dingens@gmail.com>
2024-11-30 12:31:42 +0100

    p1l9

--- startcode/main.py
+++ startcode/main.py
@@ -1,12 +1,24 @@
 import pygame, sys
+#from pygame.examples.video import backgrounds
 from pygame.locals import QUIT
-
+kleur = (0,0,0)
 pygame.init()
-venster = pygame.display.set_mode((200, 50))
-
+venster = pygame.display.set_mode((999, 300))
+venster.fill((255,255,255))
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if pygame.mouse.get_pressed() == (True, False, False):
+            pygame.draw.circle(venster,  kleur, pygame.mouse.get_pos(),6)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                kleur = (255,0,0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_b:
+                kleur = (0,0,0)
+    if pygame.mouse.get_pressed() == (False, False, True):
+        pygame.draw.circle(venster, (255,255,255), pygame.mouse.get_pos(), 6)
     pygame.display.update()
+
